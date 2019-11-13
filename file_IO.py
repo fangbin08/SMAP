221c3da0f74e22dca8ba116e1b6ab9480b3d191b
Bin Fang <bf2369@columbia.edu>
2019-11-12 20:51:56 -0500

    Update file_IO.py

--- file_IO.py
+++ file_IO.py
@@ -6,3 +6,26 @@ for x in range(len(varname_list)):
     var_obj = f[varname_list[x]][()]
     exec(varname_list[x] + '= var_obj')
 f.close()
+
+####################################################################################################################################
+# * Check the completeness of downloaded files
+os.chdir(path_ltdr)
+files = sorted(glob.glob('*.nc4'))
+# date_seq_late = date_seq[6939:]
+files_group = []
+for idt in range(13879):
+    # files_group_1day = [files.index(i) for i in files if 'A' + date_seq_late[idt] in i]
+    files_group_1day = [files.index(i) for i in files if 'A' + date_seq[idt] in i]
+    files_group.append(files_group_1day)
+    print(idt)
+
+file_miss = []
+for idt in range(len(files_group)):
+    if len(files_group[idt]) != 8:
+        file_miss.append(date_seq[idt])
+        print(date_seq[idt])
+        # file_miss.append(date_seq_late[idt])
+        # print(date_seq_late[idt])
+        print(len(files_group[idt]))
+    else:
+        pass

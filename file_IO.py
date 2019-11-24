acca9d4bcd4b5a069e294612410de5233f729e77
Bin Fang <bf2369@columbia.edu>
2019-11-24 12:45:52 -0500

    Update file_IO.py

--- file_IO.py
+++ file_IO.py
@@ -42,10 +42,22 @@ for m in range(len(month_num)):
     ind_month_gldas.append(ind_month_gldas_1mo)
 
 # Initialize empty matrices
-# lst_gldas_am_delta_all = np.empty((len(row_lmask_ease_25km_ind), 0), float).astype('float32')
-# lst_gldas_pm_delta_all = np.empty((len(row_lmask_ease_25km_ind), 0), float).astype('float32')
-# sm_gldas_am_all = np.empty((len(row_lmask_ease_25km_ind), 0), float).astype('float32')
-# sm_gldas_pm_all = np.empty((len(row_lmask_ease_25km_ind), 0), float).astype('float32')
-# ltdr_ndvi_all = np.empty((len(row_lmask_ease_25km_ind), 0), float).astype('float32')
+lst_gldas_am_delta_all = np.empty((len(row_lmask_ease_25km_ind), 0), float).astype('float32')
+lst_gldas_pm_delta_all = np.empty((len(row_lmask_ease_25km_ind), 0), float).astype('float32')
+sm_gldas_am_all = np.empty((len(row_lmask_ease_25km_ind), 0), float).astype('float32')
+sm_gldas_pm_all = np.empty((len(row_lmask_ease_25km_ind), 0), float).astype('float32')
+ltdr_ndvi_all = np.empty((len(row_lmask_ease_25km_ind), 0), float).astype('float32')
     
-    
+            os.chdir(path_modis_model + modis_folders[ifo] + subfolders[iyr])
+            if modis_mat_ease.shape[2] == 2: #MODIS LST
+                var_name = ['modis_lst_1km_day_' + mos_file_name[-7:], 'modis_lst_1km_night_' + mos_file_name[-7:]]
+                with h5py.File('modis_lst_1km_' + mos_file_name[-7:] + '.hdf5', 'w') as f:
+                    for idv in range(modis_mat_ease.shape[2]):
+                        f.create_dataset(var_name[idv], data=modis_mat_ease[:, :, idv])
+                f.close()
+            else: # MODIS NDVI
+                var_name = ['modis_ndvi_1km_' + mos_file_name[-7:]]
+                with h5py.File('modis_ndvi_1km_' + mos_file_name[-7:] + '.hdf5', 'w') as f:
+                    for idv in range(modis_mat_ease.shape[2]):
+                        f.create_dataset(var_name[idv], data=modis_mat_ease[:, :, idv])
+                f.close()

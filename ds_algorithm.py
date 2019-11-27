eb6619c51a62f37882abc9c3053bfec8fcab7a61
Bin Fang <bf2369@columbia.edu>
2019-11-27 01:00:26 -0500

    Add files via upload

--- ds_algorithm.py
+++ ds_algorithm.py
@@ -34,6 +34,7 @@ path_smap_sm_ds = '/Volumes/MyPassport/SMAP_Project/Datasets/SMAP_ds/Model'
 
 lst_folder = '/MYD11A1/'
 ndvi_folder = '/MYD13A2/'
+smap_sm_9km_name = ['smap_sm_9km_am', 'smap_sm_9km_pm']
 
 # Load in geo-location parameters
 os.chdir(path_workspace)
@@ -41,8 +42,8 @@ f = h5py.File("ds_parameters.hdf5", "r")
 varname_list = ['lat_world_max', 'lat_world_min', 'lon_world_max', 'lon_world_min',
                 'lat_world_ease_9km', 'lon_world_ease_9km', 'lat_world_ease_1km', 'lon_world_ease_1km',
                 'lat_world_ease_25km', 'lon_world_ease_25km', 'row_world_ease_1km_from_25km_ind',
-                'col_world_ease_1km_from_25km_ind', 'row_world_ease_9km_from_1km_ext33km_ind',
-                'col_world_ease_9km_from_1km_ext33km_ind']
+                'col_world_ease_1km_from_25km_ind', 'row_world_ease_1km_from_9km_ind', 'col_world_ease_1km_from_9km_ind',
+                'row_world_ease_9km_from_1km_ext33km_ind', 'col_world_ease_9km_from_1km_ext33km_ind']
 
 for x in range(len(varname_list)):
     var_obj = f[varname_list[x]][()]
@@ -103,6 +104,15 @@ modis_ndvi_days_ind = np.array([modis_ndvi_days[np.where(np.absolute(oneyeardays
                                                 np.amin((np.absolute(oneyeardays_sq[x] - modis_ndvi_days))))].item(0)
                        for x in range(len(oneyeardays_sq))])
 
+# Convert the 1 km from 9 km/25 km match table files to 1-d linear
+col_meshgrid_from_9km, row_meshgrid_from_9km = np.meshgrid(col_world_ease_1km_from_9km_ind, row_world_ease_1km_from_9km_ind)
+col_meshgrid_from_9km = col_meshgrid_from_9km.reshape(1, -1)
+row_meshgrid_from_9km = row_meshgrid_from_9km.reshape(1, -1)
+
+col_meshgrid_from_25km, row_meshgrid_from_25km = np.meshgrid(col_world_ease_1km_from_25km_ind, row_world_ease_1km_from_25km_ind)
+col_meshgrid_from_25km = col_meshgrid_from_25km.reshape(1, -1)
+row_meshgrid_from_25km = row_meshgrid_from_25km.reshape(1, -1)
+
 
 ########################################################################################################################
 # 1. Implement the VIS/IR downscaling model on MODIS LST difference to calculate 1 km soil moisture
@@ -119,13 +129,10 @@ for x in range(len(varname_list)):
 f.close()
 
 
-# Implement the VIS/IR downscaling model on MODIS LST difference to calculate 1 km soil moisture
-
 matsize_smap_sm_model_1day = [len(lat_world_ease_1km), len(lon_world_ease_1km)]
 smap_sm_model_mat_init_1day = np.empty(matsize_smap_sm_model_1day, dtype='float32')
 smap_sm_model_mat_init_1day[:] = np.nan
 smap_sm_model_mat_init_1day_1dim = smap_sm_model_mat_init_1day.reshape(1, -1)
-
 
 for iyr in range(len(yearname)):
 
@@ -143,7 +150,7 @@ for iyr in range(len(yearname)):
         else:
             modis_lst_file_path_2 = 'None'
 
-
+        # Find the if the files exist in the directory
         if os.path.exists(modis_lst_file_path_1) == True and os.path.exists(modis_lst_file_path_2) == True:
 
             ds_lst_1 = gdal.Open(modis_lst_file_path_1)
@@ -176,9 +183,6 @@ for iyr in range(len(yearname)):
 
 
             # Extract coefficient and intercept from the position indices of corresponding monthly model file
-            col_meshgrid, row_meshgrid = np.meshgrid(col_world_ease_1km_from_25km_ind, row_world_ease_1km_from_25km_ind)
-            col_meshgrid = col_meshgrid.reshape(1, -1)
-            row_meshgrid = row_meshgrid.reshape(1, -1)
             ds_ndvi_idx = ds_ndvi_idx.reshape(1, -1)
 
             month_id = str(datetime.datetime.strptime(str(yearname[iyr]) + '+' + str(idt+1).zfill(3), '%Y+%j').month).zfill(2)
@@ -186,10 +190,12 @@ for iyr in range(len(yearname)):
             exec('coef_mat_pm = ' + 'coef_mat_pm_' + month_id)
 
             # AM SM
-            coef_mat_am_coef = np.array([coef_mat_am[row_meshgrid[0, ds_lst_am_ind[x]], col_meshgrid[0, ds_lst_am_ind[x]],
-                                               ds_ndvi_idx[0, ds_lst_am_ind[x]]*2] for x in range(len(ds_lst_am_ind))])
-            coef_mat_am_intc = np.array([coef_mat_am[row_meshgrid[0, ds_lst_am_ind[x]], col_meshgrid[0, ds_lst_am_ind[x]],
-                                               ds_ndvi_idx[0, ds_lst_am_ind[x]]*2+1] for x in range(len(ds_lst_am_ind))])
+            coef_mat_am_coef = np.array([coef_mat_am[row_meshgrid_from_25km[0, ds_lst_am_ind[x]],
+                                                     col_meshgrid_from_25km[0, ds_lst_am_ind[x]],
+                                                     ds_ndvi_idx[0, ds_lst_am_ind[x]]*2] for x in range(len(ds_lst_am_ind))])
+            coef_mat_am_intc = np.array([coef_mat_am[row_meshgrid_from_25km[0, ds_lst_am_ind[x]],
+                                                     col_meshgrid_from_25km[0, ds_lst_am_ind[x]],
+                                                     ds_ndvi_idx[0, ds_lst_am_ind[x]]*2+1] for x in range(len(ds_lst_am_ind))])
             smap_sm_1km_am_model_nonnan = coef_mat_am_coef * ds_lst_am_nonnan + coef_mat_am_intc
 
             smap_sm_1km_am_model = np.copy(smap_sm_model_mat_init_1day_1dim)
@@ -198,10 +204,12 @@ for iyr in range(len(yearname)):
             smap_sm_1km_am_model = smap_sm_1km_am_model.reshape(matsize_smap_sm_model_1day)
 
             # PM SM
-            coef_mat_pm_coef = np.array([coef_mat_pm[row_meshgrid[0, ds_lst_pm_ind[x]], col_meshgrid[0, ds_lst_pm_ind[x]],
-                                               ds_ndvi_idx[0, ds_lst_pm_ind[x]]*2] for x in range(len(ds_lst_pm_ind))])
-            coef_mat_pm_intc = np.array([coef_mat_pm[row_meshgrid[0, ds_lst_pm_ind[x]], col_meshgrid[0, ds_lst_pm_ind[x]],
-                                               ds_ndvi_idx[0, ds_lst_pm_ind[x]]*2+1] for x in range(len(ds_lst_pm_ind))])
+            coef_mat_pm_coef = np.array([coef_mat_pm[row_meshgrid_from_25km[0, ds_lst_pm_ind[x]],
+                                                     col_meshgrid_from_25km[0, ds_lst_pm_ind[x]],
+                                                     ds_ndvi_idx[0, ds_lst_pm_ind[x]]*2] for x in range(len(ds_lst_pm_ind))])
+            coef_mat_pm_intc = np.array([coef_mat_pm[row_meshgrid_from_25km[0, ds_lst_pm_ind[x]],
+                                                     col_meshgrid_from_25km[0, ds_lst_pm_ind[x]],
+                                                     ds_ndvi_idx[0, ds_lst_pm_ind[x]]*2+1] for x in range(len(ds_lst_pm_ind))])
             smap_sm_1km_pm_model_nonnan = coef_mat_pm_coef * ds_lst_pm_nonnan + coef_mat_pm_intc
 
             smap_sm_1km_pm_model = np.copy(smap_sm_model_mat_init_1day_1dim)
@@ -210,8 +218,8 @@ for iyr in range(len(yearname)):
             smap_sm_1km_pm_model = smap_sm_1km_pm_model.reshape(matsize_smap_sm_model_1day)
 
             del(ds_lst_am, ds_lst_pm, ds_lst_am_ind, ds_lst_pm_ind, ds_lst_am_nonnan, ds_lst_pm_nonnan, ds_ndvi_idx,
-                col_meshgrid, row_meshgrid, month_id, coef_mat_am_coef, coef_mat_am_intc, smap_sm_1km_am_model_nonnan,
-                coef_mat_pm_coef, coef_mat_pm_intc, smap_sm_1km_pm_model_nonnan)
+                month_id, coef_mat_am_coef, coef_mat_am_intc, smap_sm_1km_am_model_nonnan, coef_mat_pm_coef,
+                coef_mat_pm_intc, smap_sm_1km_pm_model_nonnan)
 
 
             # Save the daily 1 km SM model output to Geotiff files
@@ -241,105 +249,90 @@ for iyr in range(len(yearname)):
 
 
 
+########################################################################################################################
+# 2. Implement the VIS/IR downscaling model on MODIS LST difference to calculate 1 km soil moisture
+
+# Create initial EASE grid projection matrices
+smap_sm_1km_agg_init = np.empty([len(lat_world_ease_9km), len(lon_world_ease_9km)], dtype='float32')
+smap_sm_1km_agg_init[:] = np.nan
+smap_sm_1km_disagg_init = np.empty([len(lat_world_ease_1km), len(lon_world_ease_1km)], dtype='float32')
+smap_sm_1km_disagg_init[:] = np.nan
+
+for iyr in range(len(yearname)):
+
+    for imo in range(len(monthname)):
+
+        # Load in SMAP 9km SM data
+        smap_file_path = path_procdata + '/smap_sm_9km_' + str(yearname[iyr]) + monthname[imo] + '.hdf5'
+
+        # Find the if the file exists in the directory
+        if os.path.exists(smap_file_path) == True:
+            f_smap_9km = h5py.File(smap_file_path, "r")
+            varname_list_smap_ip = list(f_smap_9km.keys())
+            for x in range(len(varname_list_smap_ip)):
+                var_obj = f_smap_9km[varname_list_smap_ip[x]][()]
+                exec(smap_sm_9km_name[x] + '= var_obj')
+                del(var_obj)
+            f_smap_9km.close()
+
+        smap_sm_9km = np.concatenate((smap_sm_9km_am, smap_sm_9km_pm), axis=2)
+        del(smap_sm_9km_am, smap_sm_9km_pm)
+
+        # Load in MODIS LST data
+        month_begin = daysofmonth_seq[0:imo, iyr].sum()
+        month_end = daysofmonth_seq[0:imo + 1, iyr].sum()
+        month_lenth = month_end - month_begin
+        for idt in range(month_lenth):
+
+            smap_sm_1km_file_path = path_model_op + '/' + str(yearname[iyr]) + '/smap_sm_1km_' + str(yearname[iyr]) + \
+                                    str(month_begin+idt+1).zfill(3) + '.tif'
+
+            if os.path.exists(smap_sm_1km_file_path) == True:
+                ds_smap_sm_1km = gdal.Open(smap_sm_1km_file_path)
+                smap_sm_1km = ds_smap_sm_1km.ReadAsArray()
+                smap_sm_1km = np.transpose(smap_sm_1km, (1, 2, 0))
+
+            for idf in range(2):
+                # Aggregate 1km SM model output to 9 km resolution, and calculate its difference with 9 km SMAP SM
+                smap_sm_1km_agg = np.copy(smap_sm_1km_agg_init)
+                smap_sm_1km_1file = smap_sm_1km[:, :, idf]
+                smap_sm_1km_agg = np.array \
+                    ([np.nanmean(smap_sm_1km_1file[row_world_ease_9km_from_1km_ext33km_ind[x], :], axis=0)
+                      for x in range(len(lat_world_ease_9km))])
+                smap_sm_1km_agg = np.array \
+                    ([np.nanmean(smap_sm_1km_agg[:, col_world_ease_9km_from_1km_ext33km_ind[y]], axis=1)
+                      for y in range(len(lon_world_ease_9km))])
+                smap_sm_1km_agg = np.fliplr(np.rot90(smap_sm_1km_agg, 3))
+                smap_sm_1km_delta = smap_sm_9km[:, :, month_lenth*idf+idt] - smap_sm_1km_agg
+                smap_sm_1km_delta = smap_sm_1km_delta.reshape(1, -1)
+                # [row_smap_sm_1km_delta, col_smap_sm_1km_delta] = np.where(~np.isnan(smap_sm_1km_delta))
+                # smap_sm_1km_delta_ind = np.where(~np.isnan(smap_sm_1km_delta))[1]
+
+
+                smap_sm_1km_1file_1dim = smap_sm_1km_1file.reshape(1, -1)
+                smap_sm_1km_1file_ind = np.where(~np.isnan(smap_sm_1km_1file_1dim))[1]
+                smap_sm_1km_delta_disagg = np.array([smap_sm_1km_delta[row_meshgrid_from_9km[0, smap_sm_1km_1file_ind[x]],
+                                          col_meshgrid_from_9km[0, smap_sm_1km_1file_ind[x]]]
+                              for x in range(len(smap_sm_1km_1file_ind))])
+                smap_sm_1km_disagg = np.copy(smap_sm_1km_disagg_init)
+                smap_sm_1km_disagg = smap_sm_1km_disagg.reshape(1, -1)
+                smap_sm_1km_disagg[0, smap_sm_1km_1file_ind] = smap_sm_1km_delta_disagg
+                smap_sm_1km_disagg = smap_sm_1km_disagg.reshape(len(lat_world_ease_1km), len(lon_world_ease_1km))
+                smap_sm_1km_ds = smap_sm_1km_1file + smap_sm_1km_disagg
+                smap_sm_1km_ds[np.where(smap_sm_1km_ds <= 0)] = np.nan
 
 
 
 
 
-# for iyr in range(len(yearname)):
-#
-#     for imo in range(len(monthname)):
-#
-#         # Load in SMAP 9km SM data
-#         smap_file_path = path_procdata + '/smap_sm_9km_' + str(yearname[iyr]) + monthname[imo] + '.hdf5'
-#         f_smap_ip = h5py.File(smap_file_path, "r")
-#         varname_list_smap_ip = list(f_smap_ip.keys())
-#         for x in range(len(varname_list_smap_ip)):
-#             var_obj = f_smap_ip[varname_list_smap_ip[x]][()]
-#             exec(varname_list_smap_ip[x] + '= var_obj')
-#             del(var_obj)
-#         f_smap_ip.close()
-#
-#         month_begin = daysofmonth_seq[0:imo, 4].sum()
-#         month_end = daysofmonth_seq[0:imo + 1, 4].sum()
-#         for idt in range(month_begin, month_end):
-#             # Load in MODIS LST data
-#             modis_lst_file_path_1 = path_modis_model_ip + lst_folder + str(yearname[iyr]) + '/' + \
-#                                     str(yearname[iyr]) + str(month_begin+idt+1).zfill(3) + '.tif'
-#             if imo < len(monthname)-1:
-#                 modis_lst_file_path_2 = path_modis_model_ip + lst_folder + str(yearname[iyr]) + '/' + \
-#                                         str(yearname[iyr]) + str(month_begin+idt+2).zfill(3) + '.tif'
-#             elif iyr < len(yearname)-1:
-#                 modis_lst_file_path_2 = path_modis_model_ip + lst_folder + str(yearname[iyr+1]) + '/' + \
-#                                         str(yearname[iyr+1]) + str(1).zfill(3) + '.tif'
-#             else:
-#                 pass
-#
-#             # Load in MODIS NDVI data
+                # smap_sm_1km_delta_disagg = np.array(
+                #     [smap_sm_1km_delta[row_meshgrid_from_9km[0, x], col_meshgrid_from_9km[0, x]]
+                #      for x in range(row_meshgrid_from_9km.shape[1])])
 
 
+                # smap_sm_1km_am_model_nonnan = coef_mat_am_coef * ds_lst_am_nonnan + coef_mat_am_intc
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# model_coeff_size = model_coeff_am.shape
-# lon_meshgrid, lat_meshgrid = np.meshgrid(col_conus_ease_1km_from_12_5km_ind, row_conus_ease_1km_from_12_5km_ind)
-#
-# a = model_coeff_am[lon_meshgrid, lat_meshgrid, 0, 0]
-#
-# model_coeff_am_1dim = model_coeff_am.reshape(-1, 20, 6)
-# lon_meshgrid_1dim = lon_meshgrid.reshape(-1, 1)
-# lat_meshgrid_1dim = lat_meshgrid.reshape(-1, 1)
-#
-# # a = [model_coeff_am[lat_meshgrid, lon_meshgrid, 0, 0] for lon_meshgrid_1dim in range(len(lon_meshgrid_1dim))]
-
-
-# Downscale the model coefficients to 1 km resolution
-# model_coeff_am_1km = np.zeros((lat_conus_ease_1km.shape[1], lon_conus_ease_1km.shape[1],
-#                                (len(ndvi_interval))*2, len(month_list)))
-# model_coeff_am_1km.fill(np.nan)
-# # model_coeff_am_1km_1dim = model_coeff_am_1km.reshape(-1, 20, 6)
-#
-# for s in range(lat_conus_ease_1km.shape[1]):
-#     for t in range(lon_conus_ease_1km.shape[1]):
-#         model_coeff_am_1km[s, t, :, :] = \
-#             model_coeff_am[row_conus_ease_1km_from_12_5km_ind[0, s], col_conus_ease_1km_from_12_5km_ind[0, t], :, :]
-#         # print([s, t])
-#
-# model_coeff_am_1km_1dim = model_coeff_am_1km.reshape(-1, 22, 6)
-
-# b = [model_coeff_am_1km_1dim[model_coeff_am[lat_meshgrid[x], lon_meshgrid[x], :, :]] for x in range(100)]
-
-# model_coeff_am_1km_singleday = model_coeff_am_1km[:, :, 0, 0]
-# model_coeff_am_1km_singleday = [model_coeff_am[lat_meshgrid, lon_meshgrid, 0, 0] for lon_meshgrid_1dim in range(len(lon_meshgrid_1dim))]
-
-
-# ndvi_input_mat = modis_ndvi_1km_index[:, :].reshape(1, -1)
-# a = [model_coeff_am_1km_1dim[x, ndvi_input_mat[0, x], i] for x in range(len(lon_meshgrid_1dim))]
-
-
-# sm_1km_model_am = model_coeff_am[] * sm_1km_ds_2015_4_am[:, :, i] + model_coeff_am[]
-
-
-
-
-
-
-
-
+                # modis_mat_ease[:, :, idm] = smap_sm_1km_agg
+                # del(modis_mat_ease_1day, modis_mat_1day)
 
